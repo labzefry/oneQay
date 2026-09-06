9d45538f1d4f090352e243f1d6fada5104665560
Profukc <lab.zefa@gmail.com>
2026-09-06 15:03:49 +0700

    Sprint96: register Final Shift Close provider
    Author by Lab | zefry

--- apps/web/bootstrap/providers.php
+++ apps/web/bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\FinalShiftCloseServiceProvider::class,
     App\Providers\TechnicalPreviewServiceProvider::class,
     App\Providers\SystemUpdateServiceProvider::class,
 ];
