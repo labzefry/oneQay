9a7d0c8d59bde153882a714f604e548d42f94fb9
Profukc <lab.zefa@gmail.com>
2026-09-06 15:07:29 +0700

    Sprint96: register Final Shift Close bootstrap provider
    Author by Lab | zefry

--- apps/web/bootstrap/app.php
+++ apps/web/bootstrap/app.php
@@ -7,11 +7,15 @@ use App\Delivery\Http\Middleware\RequireFirstPartySessionControlMutationContextM
 use App\Delivery\Http\Middleware\RequireVerifiedTenantContextMiddleware;
 use App\Delivery\Http\Middleware\SafeRequestObservationMiddleware;
 use App\Delivery\Http\Middleware\SecurityHeadersMiddleware;
+use App\Providers\FinalShiftCloseServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        FinalShiftCloseServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
