7422b707df6d0a4eea03dff914b7a40775f8df99
Profukc <lab.zefa@gmail.com>
2026-09-07 16:19:43 +0700

    Sprint124: add default-off DB binding attestation config

---
+++ apps/web/config/final_shift_close_runtime_db_binding_attestation.php
@@ -0,0 +1,7 @@
+<?php
+
+// Author by Lab | zefry
+return [
+    'enabled' => env('ONEQAY_FINAL_SHIFT_CLOSE_RUNTIME_DB_BINDING_ATTESTATION_ENABLED', false),
+    'token' => env('ONEQAY_FINAL_SHIFT_CLOSE_RUNTIME_DB_BINDING_ATTESTATION_TOKEN', ''),
+];
