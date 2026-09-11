2a859ce241f43f88dbf37a23c3fcd1ae23b337bc
Profukc <lab.zefa@gmail.com>
2026-09-11 15:23:37 +0700

    Sprint130: integrate canonical token policy in materialization middleware
    Author by Lab | zefry

--- apps/web/app/Delivery/Http/Middleware/RequireFinalShiftCloseRuntimeBindingMaterializationTokenMiddleware.php
+++ apps/web/app/Delivery/Http/Middleware/RequireFinalShiftCloseRuntimeBindingMaterializationTokenMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Delivery\Http\Middleware;
 
+use App\Application\Pos\FinalShiftCloseRuntimeControlPlaneTokenPolicy;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,18 +16,15 @@ final class RequireFinalShiftCloseRuntimeBindingMaterializationTokenMiddleware
 
     public function handle(Request $request, Closure $next): Response
     {
-        if (strlen($this->expectedToken) < 32
-            || strlen($this->expectedToken) > 512
-            || preg_match('/\A[A-Za-z0-9._~+=\/-]{32,512}\z/D', $this->expectedToken) !== 1
-        ) {
+        if (! FinalShiftCloseRuntimeControlPlaneTokenPolicy::isValidToken($this->expectedToken)) {
             abort(503);
         }
 
-        $authorization = $request->headers->get('Authorization');
-        if (! is_string($authorization)
-            || preg_match('/\ABearer ([A-Za-z0-9._~+=\/-]{32,512})\z/D', $authorization, $matches) !== 1
-            || ! hash_equals($this->expectedToken, $matches[1])
-        ) {
+        $providedToken = FinalShiftCloseRuntimeControlPlaneTokenPolicy::parseBearerCredential(
+            $request->headers->get('Authorization'),
+        );
+
+        if ($providedToken === null || ! hash_equals($this->expectedToken, $providedToken)) {
             abort(401);
         }
 
