c7004148479ee1b61a11808353ff9f1b1d9452b9
Profukc <lab.zefa@gmail.com>
2026-09-14 23:34:51 +0700

    Sprint164: compose replenishment permission in POS hub

--- apps/web/app/Application/Pos/ViewPosOperationsHub.php
+++ apps/web/app/Application/Pos/ViewPosOperationsHub.php
@@ -35,6 +35,7 @@ final readonly class ViewPosOperationsHub
             || $this->authorization->allows($verified, PosPermission::refundSale());
         $canCatalogInventorySetup = $this->authorization->allows($verified, PosPermission::prepareCatalog())
             && $this->authorization->allows($verified, PosPermission::inventoryBaseline());
+        $canInventoryReplenishment = $this->authorization->allows($verified, PosPermission::inventoryReplenish());
         $canCashVarianceReconciliation = $this->authorization->allows(
             $verified,
             PosPermission::recordCashVarianceExplanation(),
@@ -54,6 +55,7 @@ final readonly class ViewPosOperationsHub
             $canReporting,
             $canCorrections,
             $canCatalogInventorySetup,
+            $canInventoryReplenishment,
             $canCashVarianceReconciliation,
             $canShiftClose,
         );
