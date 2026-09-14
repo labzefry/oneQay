55de134fc7f3e1fa96bc0d5f38ecfeda96a5b52c
Profukc <lab.zefa@gmail.com>
2026-09-14 12:32:00 +0700

    Sprint156: register isolated POS operational reporting provider

--- apps/web/bootstrap/providers.php
+++ apps/web/bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\PosOperationalReportingServiceProvider::class,
     App\Providers\TechnicalPreviewServiceProvider::class,
     App\Providers\SystemUpdateServiceProvider::class,
 ];
