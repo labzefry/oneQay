aa4a243601499c781785e1c47ab80df29344fc93
Profukc <lab.zefa@gmail.com>
2026-09-14 23:33:01 +0700

    Sprint164: add deny-by-default inventory replenishment permission

--- apps/web/app/Application/Authorization/PosPermission.php
+++ apps/web/app/Application/Authorization/PosPermission.php
@@ -18,6 +18,7 @@ final class PosPermission
     public const VOID_SALE = 'pos.sale.void';
     public const REFUND_SALE = 'pos.sale.refund';
     public const INVENTORY_BASELINE = 'pos.inventory.baseline';
+    public const INVENTORY_REPLENISH = 'pos.inventory.replenish';
 
     public static function completeSale(): PermissionIdentifier
     {
@@ -62,5 +63,10 @@ final class PosPermission
     public static function inventoryBaseline(): PermissionIdentifier
     {
         return PermissionIdentifier::fromString(self::INVENTORY_BASELINE);
+    }
+
+    public static function inventoryReplenish(): PermissionIdentifier
+    {
+        return PermissionIdentifier::fromString(self::INVENTORY_REPLENISH);
     }
 }
