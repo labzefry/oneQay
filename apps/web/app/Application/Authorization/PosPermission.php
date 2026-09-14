0110179c939d35bc21009c6a52ace51e204e9b3b
Profukc <lab.zefa@gmail.com>
2026-09-14 12:31:06 +0700

    Sprint156: restore canonical POS permission owner

--- apps/web/app/Application/Authorization/PosPermission.php
+++ apps/web/app/Application/Authorization/PosPermission.php
@@ -18,7 +18,6 @@ final class PosPermission
     public const VOID_SALE = 'pos.sale.void';
     public const REFUND_SALE = 'pos.sale.refund';
     public const INVENTORY_BASELINE = 'pos.inventory.baseline';
-    public const VIEW_OPERATIONAL_SALES_SUMMARY = 'pos.reporting.sales-summary.view';
 
     public static function completeSale(): PermissionIdentifier
     {
@@ -63,10 +62,5 @@ final class PosPermission
     public static function inventoryBaseline(): PermissionIdentifier
     {
         return PermissionIdentifier::fromString(self::INVENTORY_BASELINE);
-    }
-
-    public static function viewOperationalSalesSummary(): PermissionIdentifier
-    {
-        return PermissionIdentifier::fromString(self::VIEW_OPERATIONAL_SALES_SUMMARY);
     }
 }
