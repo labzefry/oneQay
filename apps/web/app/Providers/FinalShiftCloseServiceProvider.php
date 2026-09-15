334521441f615df383cc160eae8390fdeb04fd2b
Profukc <lab.zefa@gmail.com>
2026-09-15 07:08:53 +0700

    Sprint164: preserve final shift close provider ownership

--- apps/web/app/Providers/FinalShiftCloseServiceProvider.php
+++ apps/web/app/Providers/FinalShiftCloseServiceProvider.php
@@ -31,7 +31,6 @@ final class FinalShiftCloseServiceProvider extends ServiceProvider
         $this->app->register(PosShiftStartWorkspaceServiceProvider::class);
         $this->app->register(PosSaleCorrectionWorkspaceServiceProvider::class);
         $this->app->register(PosCatalogInventorySetupWorkspaceServiceProvider::class);
-        $this->app->register(PosInventoryReplenishmentWorkspaceServiceProvider::class);
         $this->app->register(PosCashVarianceReconciliationWorkspaceServiceProvider::class);
         $this->app->register(PosOperationsHubServiceProvider::class);
 
