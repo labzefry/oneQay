4567a505d5beaf00d0779da3007ba98428ddb229
Profukc <lab.zefa@gmail.com>
2026-09-19 14:44:13 +0700

    Sprint198: register guarded POS delivery aggregate

--- apps/web/bootstrap/providers.php
+++ apps/web/bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
+// Author by Lab | zefry
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TechnicalPreviewServiceProvider::class,
     App\Providers\SystemUpdateServiceProvider::class,
+    App\Providers\PosOperationsHubServiceProvider::class,
 ];
